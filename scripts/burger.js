9718a356c0297b07b8970b94e1b91d76dd46af30
adammarostica <10792897+adammarostica@users.noreply.github.com>
2022-05-14 20:56:37 -0400

    Refactored media queries to accommodate for burger menu changes in mobile.

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,20 +1,19 @@
 
 const menuButton = document.getElementById('burgerMenu');
 const burgerIcon = document.getElementById('burgerIcon')
-console.log(burgerIcon);
 const navList = document.getElementById('navList');
-console.log(navList.className);
+
 menuButton.addEventListener('click', function(e) {
+  
   if (navList.className === 'revealMenu') {
     navList.classList.replace('revealMenu', 'swipeMenu');
     setTimeout(function () {
       navList.classList.remove('swipeMenu');
     }, 300);
-  }
-  if (navList.className === '') {
+  } else if (navList.className === '') {
     navList.classList.toggle('revealMenu');
   }
-  console.log(navList.className);
+
   this.classList.toggle('expandBurger')
   burgerIcon.children[0].classList.toggle('changeTop');
   burgerIcon.children[1].classList.toggle('changeMid');
