1b844d967c30d55cc4e4e49b1e8a08632523cebb
adammarostica <10792897+adammarostica@users.noreply.github.com>
2022-05-15 08:52:08 -0400

    burger menu animations, added burger meny to contact and blog pages

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,20 +1,22 @@
 
 const menuButton = document.getElementById('burgerMenu');
-const burgerIcon = document.getElementById('burgerIcon')
+const burgerIcon = document.getElementById('burgerIcon');
 const navList = document.getElementById('navList');
 
 menuButton.addEventListener('click', function(e) {
   
-  if (navList.className === 'revealMenu') {
-    navList.classList.replace('revealMenu', 'swipeMenu');
-    setTimeout(function () {
-      navList.classList.remove('swipeMenu');
-    }, 300);
+  if (navList.className === 'showNav') {
+    navList.classList.replace('showNav', 'hideNav');
+    this.classList.replace('growMenu', 'shrinkMenu');
+    setTimeout(() => {
+      navList.classList.remove('hideNav');
+      this.classList.remove('shrinkMenu');
+    }, 600);
   } else if (navList.className === '') {
-    navList.classList.toggle('revealMenu');
+    navList.classList.toggle('showNav');
+    this.classList.add('growMenu');
   }
 
-  this.classList.toggle('expandBurger')
   burgerIcon.children[0].classList.toggle('changeTop');
   burgerIcon.children[1].classList.toggle('changeMid');
   burgerIcon.children[2].classList.toggle('changeBtm');
